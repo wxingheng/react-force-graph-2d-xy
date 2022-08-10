eddd83bd24ccd75902a3a476e31036eb85aa81a4
wxingheng <wxingheng@outlook.com>
2022-08-10 15:19:07 +0800

    Feat init project

---
+++ index.js
@@ -0,0 +1,28 @@
+import fromKapsule from 'react-kapsule';
+import ForceGraph2DKapsule from 'force-graph-xy';
+import { ForceGraph2DPropTypes } from '../../forcegraph-proptypes';
+
+const ForceGraph2D = fromKapsule(
+  ForceGraph2DKapsule,
+  {
+    methodNames: [ // bind methods
+      'emitParticle',
+      'd3Force',
+      'd3ReheatSimulation',
+      'stopAnimation',
+      'pauseAnimation',
+      'resumeAnimation',
+      'centerAt',
+      'zoom',
+      'zoomToFit',
+      'getGraphBbox',
+      'screen2GraphCoords',
+      'graph2ScreenCoords'
+    ]
+  }
+);
+
+ForceGraph2D.displayName = 'ForceGraph2D';
+ForceGraph2D.propTypes = ForceGraph2DPropTypes;
+
+export default ForceGraph2D;
